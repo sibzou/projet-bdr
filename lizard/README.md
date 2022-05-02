16b2739da976753b89df7db37b8320866ff3460d
sibzou <91130460+sibzou@users.noreply.github.com>
2022-05-02 17:50:48 +0200

    Create the maven project for the part 3

---
+++ lizard/README.md
@@ -0,0 +1,13 @@
+# lizard
+
+La compilation du projet se fait avec Maven. Pour juste compiler :
+
+```bash
+$ mvn compile
+```
+
+Pour compiler et exécuter :
+
+```bash
+$ mvn javafx:run
+```
---
+++ lizard/src/main/java/com/lp/bdr/lizard/Main.java
@@ -0,0 +1,16 @@
+package com.lp.bdr.lizard;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("lizard");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
